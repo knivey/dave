f24e107ecedea65eba4c585ab36ed9a5a0c27865
knivey <knivey@botops.net>
2026-05-29 14:27:34 -0400

    fix: provide help_url fallback in no_context notice

--- irc_handlers.go
+++ irc_handlers.go
@@ -175,7 +175,7 @@ func handleMention(network Network, client *girc.Client, event girc.Event, chann
 		logger.Info("Ignoring message due to no existing chat context")
 		var noCtxMsg string
 		readConfig(func() { noCtxMsg = config.Notices.Mentions.NoContext })
-		noCtxMsg = expandNotice(noCtxMsg, map[string]string{"trigger": network.Trigger})
+		noCtxMsg = expandNotice(noCtxMsg, map[string]string{"trigger": network.Trigger, "help_url": network.Trigger + "help"})
 		client.Cmd.Reply(event, warnMsg(noCtxMsg))
 		return
 	}
